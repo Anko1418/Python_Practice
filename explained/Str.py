42bcabbce5bf1b98c6742983491740d6c6d9a3dd
Anko1418 <ak.aniketkokate@gmail.com>
2018-02-16 20:33:44 -0800

    explained package commit

--- explained/Str.py
+++ explained/Str.py
@@ -7,3 +7,12 @@ print("a" * 5)
 # print all chracters in a string
 for char in "Python":
     print(char)
+
+it = iter("Python")
+
+print(*it)
+# prints P y t h o n
+
+print(*it)
+# prints nothing. No values to go through
+
