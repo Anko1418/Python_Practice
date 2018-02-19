8c29b3215bea97a7afe65b40e51e0898112499ad
Anko1418 <ak.aniketkokate@gmail.com>
2018-02-19 04:32:46 -0800

    importing_files package commit

--- importing_files/import_excel_file.py
+++ importing_files/import_excel_file.py
@@ -2,12 +2,18 @@
 import pandas as pd
 
 # file path
+# more excel files at http://mercury.webster.edu/aleshunas/Data%20Sets/Supplemental%20Excel%20Data%20Sets.htm
 file = 'F:\Python\DataScience\Datasets\pnorectic.xls'
 
 # copies the contents of excel file into data variable.
 # if you are getting an error for below line, open terminal or cmd and type this 'pip install xlrd'
 data = pd.ExcelFile(file)
 
+# prints the names of sheets available in excel file
 print(data.sheet_names)
+# ['anorectic']
 
-print(data.parse(0))
+# prints the contents of first sheet
+print(data.parse(data.sheet_names[0]))
+#      weight  mens  fast  binge  vomit  purge  hyper  fami  eman  frie  ...
+# 0         1     1     1      4      4      4      1     1     1     3  ...
