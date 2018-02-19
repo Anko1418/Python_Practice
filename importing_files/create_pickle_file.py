f0b5a0cf107da40d352a14ac5de9357ec84b0758
Anko1418 <ak.aniketkokate@gmail.com>
2018-02-19 04:16:17 -0800

    importing_files package commit

--- importing_files/create_pickle_file.py
+++ importing_files/create_pickle_file.py
@@ -1,5 +1,8 @@
+# import statements
 import pickle
 
+# initialization
 demo_data = {"Data1": 0, "Data2": 1, "Data3": 2}
 
+# create a file and copying demo_data contents in the file
 pickle.dump(demo_data, open("pickle_example_1.p", "wb"))
