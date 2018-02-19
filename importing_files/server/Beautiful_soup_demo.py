78986be57f34d0accb51764f5ee6f33b30b0b679
Anko1418 <ak.aniketkokate@gmail.com>
2018-02-19 07:27:04 -0800

    importing_files package commit

--- importing_files/server/Beautiful_soup_demo.py
+++ importing_files/server/Beautiful_soup_demo.py
@@ -31,3 +31,10 @@ guido_text = soup.get_text()
 
 # Print Guido's text to the shell
 print(guido_text)
+
+# Find all 'a' tags (which define hyperlinks): a_tags
+a_tags = soup.find_all('a')
+
+# Print the URLs to the shell
+for link in a_tags:
+    print(link.get('href'))
