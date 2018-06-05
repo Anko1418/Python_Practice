fa119b8c5344cbff230f7832f5ded267916006a0
Anko1418 <ak.aniketkokate@gmail.com>
2018-06-05 10:07:31 -0700

    commit- data cleaning package

--- the_math_of_intelligence/SupportVectorMachine_demo.py
+++ the_math_of_intelligence/SupportVectorMachine_demo.py
@@ -21,4 +21,34 @@ for d, sample in enumerate(X):
     else:
         plt.scatter(sample[0], sample[1], s=120, marker='+', linewidths=2)
 
+# plt.show()
+
+# Initialize our SVMs weight vector with zeros (3 values)
+w = np.zeros(len(X[0]))
+# The learning rate
+eta = 1
+# how many iterations to train for
+epochs = 100000
+# store misclassifications so we can plot how they change over time
+errors = []
+
+for epoch in range(1, epochs):
+    error = 0
+    print(epoch)
+    for i, x in enumerate(X):
+        # misclassification
+        if (y[i] * np.dot(X[i], w)) < 1:
+            # misclassified update for ours weights
+            w = w + eta * ((X[i] * y[i]) + (-2 * (1 / epoch) * w))
+            error = 1
+        else:
+            # correct classification, update our weights
+            w = w + eta * (-2 * (1 / epoch) * w)
+    errors.append(error)
+
+plt.plot(errors, '|')
+plt.ylim(0.5, 1.5)
+plt.axes().set_yticklabels([])
+plt.xlabel('Epoch')
+plt.ylabel('Misclassified')
 plt.show()
